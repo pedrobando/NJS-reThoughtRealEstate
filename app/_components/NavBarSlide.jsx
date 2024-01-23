a3f9bb22e00b7f09d567bdb7ee9f9fca7f41dadb
pedrobando <pedrobandorivera@gmail.com>
2024-01-22 22:04:59 -0600

    Change slide color opacity

--- app/_components/NavBarSlide.jsx
+++ app/_components/NavBarSlide.jsx
@@ -31,7 +31,7 @@ export default function NavBarSlide() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-gray-950 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-hidden">
