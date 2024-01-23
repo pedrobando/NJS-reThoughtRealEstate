dbd780294e8d06965857a412a9bc13ae9f2085c6
pedrobando <pedrobandorivera@gmail.com>
2024-01-22 21:10:51 -0600

    Create template for Exclusive Listings and create the call to get all the listing information.

--- app/exclusive-listings/[exclusiveId]/page.jsx
+++ app/exclusive-listings/[exclusiveId]/page.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
 
-const page = () => {
-  return (
-    <div className='text-lg text-reDark'>page</div>
-  )
-}
 
-export default page
+const getExclusiveListing = async (mlsId) => {
+    try {
+      const res = await fetch(`${process.env.HOMEJUNCTION_RE_LITING_URI}${mlsId.params.exclusiveId}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.HOMEJUNCTION_TOKEN}`,
+        },
+        cache: "no-store",
+      });
+  
+      if (!res.ok) {
+        throw new Error("Failed to fetch topics", res);
+      }
+   
+      return await res.json();
+    } catch (error) {
+      console.log("Error loading topics: ", error);
+      
+      return { result: { listings: [] } }; // Return an empty array or handle the error gracefully
+    }
+  };
+
+  
+  export default async function ExclusiveListing(exclusiveId) {
+    const { result } = await getExclusiveListing(exclusiveId);
+    const listing = result || [];
+    
+    return (
+
+        <div>Page</div>
+    )
+
+  }
