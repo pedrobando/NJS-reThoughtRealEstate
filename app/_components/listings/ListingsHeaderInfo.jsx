79e84eee1a05220e7333d3701553e8911a4ad68e
pedrobando <pedrobandorivera@gmail.com>
2024-02-17 01:34:12 -0600

    Fix code bug in exclusive listing single page parking logic.

--- app/_components/listings/ListingsHeaderInfo.jsx
+++ app/_components/listings/ListingsHeaderInfo.jsx
@@ -15,14 +15,14 @@ const ListingsHeaderInfo = ({ listing }) => {
       {listing.propertyType !== "Multi-family" && (
         <>
           {listing.beds && (
-            <div className="exclusive-listings-hero">
+            <div className="exclusive-listings-hero beds">
               <FontAwesomeIcon icon={faBed} height="20px" />
               <span className="mt-1">{listing.beds} Beds</span>
             </div>
           )}
 
           {listing.baths && (
-            <div className="exclusive-listings-hero">
+            <div className="exclusive-listings-hero baths">
               <FontAwesomeIcon icon={faBath} height="20px" />
               <span className="mt-1">
                 {listing.baths.full} / {listing.baths.half} Baths
@@ -30,8 +30,8 @@ const ListingsHeaderInfo = ({ listing }) => {
             </div>
           )}
 
-          {listing.xf_list_25 && (
-            <div className="exclusive-listings-hero">
+          {listing.xf_list_25 !== undefined && listing.xf_list_25 !== 0 &&(
+            <div className="exclusive-listings-hero parking">
               <FontAwesomeIcon icon={faCar} height="20px" />
               <span className="mt-1">{listing.xf_list_25} Parking(s)</span>
             </div>
@@ -42,7 +42,7 @@ const ListingsHeaderInfo = ({ listing }) => {
       {listing.propertyType === "Multi-family" && (
         <>
           {listing.xf_list_70 && (
-            <div className="exclusive-listings-hero">
+            <div className="exclusive-listings-hero totalunits">
               <FontAwesomeIcon icon={faHouse} height="20px" />
               <span className="mt-1">{listing.xf_list_70} Total Units</span>
             </div>
@@ -51,14 +51,14 @@ const ListingsHeaderInfo = ({ listing }) => {
       )}
       
       {listing.size ? (
-        <div className="exclusive-listings-hero">
+        <div className="exclusive-listings-hero size">
           <FontAwesomeIcon icon={faExpand} height="20px" />
           <span className="mt-1">
             {new Intl.NumberFormat().format(listing.size)} Sqft
           </span>
         </div>
       ) : (
-        <div className="exclusive-listings-hero">
+        <div className="exclusive-listings-hero lotSize">
           <FontAwesomeIcon icon={faExpand} height="20px" />
           <span className="mt-1">
             {new Intl.NumberFormat().format(listing.lotSize.sqft)} Sqft
