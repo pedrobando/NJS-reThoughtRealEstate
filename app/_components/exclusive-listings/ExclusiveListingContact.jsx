69c9c48013e007ca955666d60a39fa1fc19100b6
pedrobando <pedrobandorivera@gmail.com>
2024-03-02 02:14:56 -0600

    Add form and buttons.

--- app/_components/exclusive-listings/ExclusiveListingContact.jsx
+++ app/_components/exclusive-listings/ExclusiveListingContact.jsx
@@ -131,38 +131,7 @@ const ExclusiveListingContact = async ({ listing }) => {
           </a>
         </div>
 
-        {/* <div className=' lg:max-w-[60%] space-y-3'>
-    <label htmlFor="fullname" className="sr-only">
-        Full Name
-      </label>
-      <input
-        type="text"
-        name="fullname"
-        id="fullname"
-        className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        placeholder="Full name"
-      />
-      <label htmlFor="email" className="sr-only">
-        Email
-      </label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        placeholder="you@example.com"
-      />
-      <label htmlFor="phone" className="sr-only">
-        Phone
-      </label>
-      <input
-        type="phone"
-        name="phone-number"
-        id="phone-number"
-        className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        placeholder=" (414)000-0000"
-      />
-    </div> */}
+      
 
         <img
           src="https://rethought-realestate.com/wp-content/uploads/2021/05/Rudy-New-2-e1620845912437.png"
--- app/_components/exclusive-listings/ExclusiveListingsDrawer.jsx
+++ app/_components/exclusive-listings/ExclusiveListingsDrawer.jsx
@@ -49,7 +49,7 @@ export default function ExclusiveListingsDrawer({
           </Card>
         </div>
       </DrawerTrigger>
-      <DrawerContent className="bg-reDark !border-0 !ring-0 !outline-0 !ring-offset-0 min-h-[55%]">
+      <DrawerContent className="bg-reDark !shadow-none !border-0 !ring-0 !outline-0 !ring-offset-0 min-h-[65%] focus:ring-0 focus:shadow-none focus:border-0 focus:outline-0">
         <div className="mx-auto w-full max-w-3xl p-4">
           <DrawerHeader>
             <DrawerTitle className="text-gray-300">
@@ -57,24 +57,107 @@ export default function ExclusiveListingsDrawer({
             </DrawerTitle>
             <DrawerDescription className="text-gray-400">
               Make sure you use the best phone number and email to contact you..
-              We will call yu and email you to confirm your availability for:{" "}
+              We will call and email you check if you have any questions and to
+              confirm your availability for:{" "}
               {date ? (
                 date
               ) : (
-                <span className="font-semibold">{`${dayOfWeek} ${month} ${dayOfMonth} `}</span>
+                <span className="font-semibold text-lg">{`${dayOfWeek} ${month} ${dayOfMonth} `}</span>
               )}
             </DrawerDescription>
           </DrawerHeader>
-          <div className="p-4 pb-0"></div>
+          <form onSubmit="">
+            <fieldset disabled="" className="group">
+              <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 p-4 group-disabled:opacity-50">
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium leading-6 text-gray-200 "
+                  >
+                    First name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="bg-transparent block w-full rounded-md border-0 py-1.5 px-2 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-reGreen sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium leading-6 text-gray-200"
+                  >
+                    Last name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="bg-transparent px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-reGreen sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-200"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      className="bg-transparent px-2  block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-reGreen sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="phone"
+                    className="block text-sm font-medium leading-6 text-gray-200"
+                  >
+                    Phone Number
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="phone-number"
+                      id="phone-number"
+                      className=" bg-transparent px-2  block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-reGreen sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+          </form>
           <DrawerFooter>
-            <DrawerClose asChild>
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <DrawerClose asChild>
+                <Button
+                  variant="outline"
+                  className="text-reRed hover:text-reRed font-heading font-semibold hover:scale-95 transition"
+                >
+                  Cancel
+                </Button>
+              </DrawerClose>
               <Button
-                variant="outline"
-                className="text-reRed font-heading font-semibold"
+                type="submit"
+                className="rounded-md bg-reGreen hover:bg-reGreen px-3 py-2 text-sm font-semibold font-heading text-white shadow-sm hover:scale-95 transition"
               >
-                Cancel
+                Get Appoitment Confirmation
               </Button>
-            </DrawerClose>
+            </div>
           </DrawerFooter>
         </div>
       </DrawerContent>
