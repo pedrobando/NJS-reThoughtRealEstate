d35865e1b6572088678c07244bd1a70485614054
Pedro Rivera <pedrobandorivera@gmail.com>
2025-03-06 17:33:30 -0500

    Add bundle analizer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 module.exports = {
   generateBuildId: async () => {
     // This could be anything, using the latest git hash
